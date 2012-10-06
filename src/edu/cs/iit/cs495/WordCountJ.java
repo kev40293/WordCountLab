fd5122172979c5e120ea79e51f3114fe1aa0ab5b
Kevin Brandstatter <kjbrandstatter@gmail.com>
2012-10-06 16:49:22 -0500

    Map and reduce functions prototyped psuedo code for data flow written, need way to parallelize partitioning

--- src/edu/cs/iit/cs495/WordCountJ.java
+++ src/edu/cs/iit/cs495/WordCountJ.java
@@ -1,20 +1,25 @@
 package edu.cs.iit.cs495;
+import java.util.Map;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.io.File;
 public class WordCountJ {
-   private int numReducer = 100;
-   public WordCountJ(){
-   }
-   private class Mapper implements Runnable{
+   private class Mapper implements Runnable {
       private HashMap<String, Integer> context;
-      private String data;
-      public Mapper(String input){
-         context = new HashMap<String, Integer>();
+      private File data;
+      private int reducers;
+      public Mapper(File input,
+            HashMap<String,Integer> output){
+         context = output;
          data = input;
       }
-      private void map(String input) {
-         // tokenize
-         context.put(input, 1);
+      private void map(String word) {
+         // replace with better calculation?
+         int bucket = word.hashCode() % reducers;
+         if (context.containsKey(word))
+            context.put(word, new Integer(1));
+         else
+            context.put(word, new Integer(context.get(word).intValue() + 1));
       }
       public void run(){
          //tokenize input
@@ -28,19 +33,58 @@ public class WordCountJ {
    private class Reducer implements Runnable {
       private HashMap<String, Integer> context;
       private HashMap<String, LinkedList<Integer>> intermediate;
-      public Reducer(HashMap<String, LinkedList<Integer>> occurences){
-         context = new HashMap<String, Integer>();
+      public Reducer(HashMap<String, LinkedList<Integer>> input,
+            HashMap<String, Integer> output) {
+         context = output;
+         intermediate = input;
       }
-      private void reduce(String key, LinkedList<Integer> values){
+      private void reduce(String key){
          int sum = 0;
-         for (Integer q : values){
+         for (Integer q : intermediate.get(key)){
             sum += q.intValue();
          }
          context.put(key, sum);
       }
       public void run(){
+         /*
+          * for ( key in map )
+          * do reduce (key)
+          * done
+          *
+          * sort context
+          * return context;
+          */
       }
    }
+   private int numReducers = 100;
+   private Mapper[] mappers;
+   private HashMap<String, Integer>[][] mapout;
+   public WordCountJ(String [] filelist){
+      mappers = new Mapper[filelist.length];
+      mapout = new HashMap<String, Integer>[filelist.length][numReducers];
+      for (int i =0; i < filelist.length; i++){
+         mappers[i] = new Mapper(new File(filelist[i]), numReducers, mapout[i]);
+      }
+      /*
+       * for x in files
+       * create mapper thread, input = x;
+       * map_thread.run()
+       * done
+       *
+       * partition into n maps for reduce
+       *
+       * for n in reduce_maps
+       * thread reduce (n )
+       * done
+       *
+       * output = sort . merge reducecontex
+       *
+       *
+       * print output
+       *
+       */
+   }
+
    public static void main(String [] args){
       System.out.println("Hello World");
    }
